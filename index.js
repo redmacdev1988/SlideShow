d3caab0a217536b523404064c046bc1d86699413
Ricky Tsao <Ricky_Tsao@emap.com>
2017-10-30 15:31:54 +0800

    changed 300 to 80

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello Digital Ocean!')
 })
 
-app.listen(3000, function () {
-  console.log('Magic is happening on port 3000!')
+app.listen(80, function () {
+  console.log('Magic is happening on port 80!')
 })
