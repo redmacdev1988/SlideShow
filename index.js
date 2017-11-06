a7c0045eca8a74651a6c2de72d938f5675edfa7d
Ricky Tsao <Ricky_Tsao@emap.com>
2017-11-06 14:50:08 +0800

    serve up images

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var path = require('path')
 
 // serves up files in these two folders
 app.use(express.static(path.join(__dirname, 'views')));
-app.use(express.static(path.join(__dirname, 'Images')));
+app.use(express.static('Images'));
 
 
 console.log("index.js -  Connect your database by adding a url to the mongoose instance connection");
